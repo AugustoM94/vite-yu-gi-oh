547d02a35605498b2f3cf7106112866f7a0baa33
AugustoM94 <email@github.com>
2023-11-13 18:24:18 +0100

    Added CardComponent

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <template>
-     <HeaderApp/>
-     <MainApp/>
+      <HeaderApp/>
+   <div class="bg-main">
+        <main class="container">
+    <MainApp/>
+      </main>
+      </div>
+    <CharacterList />
 </template>
 
 <script>
+import { store } from './data/store.js';
+import axios from 'axios';
 import HeaderApp from './components/HeaderApp.vue';
-import MainApp from './components/MainApp.vue';
-
-  export default {
-    components: {
-      HeaderApp,
-      MainApp,
+import CharacterList from './components/CharacterList.vue';
+export default {
+  name: 'App',
+  components: {
+    HeaderApp,
+    CharacterList
+  },
+  data() {
+    return {
+      store
     }
-    
+  },
+  methods: {
+    getCharacters() {
+      const url = store.apiUrl + store.endPoint.character;
+      axios.get(url).then((response) => {
+        store.characterList = response.data.results;
+      })
+    }
+  },
+  created() {
+    this.getCharacters();
   }
+}
 </script>
 
 <style lang="scss" scoped>
-
-
+.bg-main {
+    background-color: #D48F38;
+    height: 100vh;
+}
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card bg-transparent border-0 ">
+        <img :src="image" class="card-img-top" :alt="title">
+        <div class="card-body text-center">
+            <h5 class="card-title">{{ title }}</h5>
+            <h6 class="card-title">{{ subtitle }}</h6>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props: [
+        'image',
+        'archetype',
+        'name',
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
